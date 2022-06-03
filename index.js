78fca4ffa9452694c6b3feb96f78cda1ee802f64
Juanqui <jcf.daw@gmail.com>
2022-06-03 17:00:25 +0200

    Tema 2 Node OB

---
+++ index.js
@@ -0,0 +1,13 @@
+const juanqui = ["Juan Carlos", 
+                27, 
+                true,
+                new Date('March 1, 1995 03:24:00'), 
+                {
+                    nombre: "Lazarillo de Tormes",
+                    autor: "Anónimo",
+                    fecha: "1554",
+                    URL: "https://www.amazon.es/Lazarillo-Tormes-clasicos-Adaptados-Cl%C3%A1sicos/dp/8431680253/ref=asc_df_8431680253/?tag=googshopes-21&linkCode=df0&hvadid=54365805435&hvpos=&hvnetw=g&hvrand=16674213670878082418&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9061040&hvtargid=pla-125455204395&psc=1"
+
+                }];
+
+console.log(juanqui)
